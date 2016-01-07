caa2f6730392056c322fe3ee59e87befb58b8103
lstr <l.sy3.21@qq.com>
2016-01-07 19:33:28 +0100

    yet another hello world
    Add Hello World to start

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<?php
+		$Start = "HelloWorld! This is the start of our Project <br />";
+		echo "$Start Just made some thing so that we don't have a empty repository!"
+	?>
+</body>
+</html>
