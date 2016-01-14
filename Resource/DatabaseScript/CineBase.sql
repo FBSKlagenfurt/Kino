589b1bd99d1906f3eb4c8a9f9f54fa4e974a240e
lstr <l.sy3.21@qq.com>
2016-01-14 15:38:55 +0100

    Fix File Location

--- Resource/DatabaseScript/CineBase.sql
+++ Resource/DatabaseScript/CineBase.sql
@@ -19,22 +19,13 @@ CREATE TABLE IF NOT EXISTS t_Stadt (
     UNIQUE uk_StadtPLZLand (PLZ, Ort, LandID)
 );
 
-CREATE TABLE IF NOT EXISTS t_Adresse (
-    ID SERIAL PRIMARY KEY,
-    Strasse VARCHAR(250) NOT NULL,
-    StrNr SMALLINT UNSIGNED NOT NULL,
-    StadtID BIGINT UNSIGNED NOT NULL,
-    CONSTRAINT fk_Adresse FOREIGN KEY (StadtID) REFERENCES t_Stadt(ID)
-);
-
 CREATE TABLE IF NOT EXISTS t_Kino(
     ID SERIAL PRIMARY KEY,
     Kinoname VARCHAR (100) NOT NULL,
     Strasse VARCHAR(250) NOT NULL,
-    PLZ VARCHAR(10) NOT NULL,
-    Stadt VARCHAR (250) NOT NULL,
-    Land VARCHAR(150) NOT NULL,
-    TelNr VARCHAR(25) NOT NULL
+    StadtID BIGINT UNSIGNED NOT NULL,
+    TelNr VARCHAR(25) NOT NULL,
+    CONSTRAINT fk_KinoAdresseStadt FOREIGN KEY (StadtID) REFERENCES t_Stadt(ID)
     /*CONSTRAINT fk_KinoAdresse FOREIGN KEY (AdresseID) REFERENCES t_Adresse(ID),*/
     /*UNIQUE uk_CinemaAddress (AddressID)*/
 );
@@ -43,17 +34,10 @@ CREATE TABLE IF NOT EXISTS t_Saal(
     ID SERIAL PRIMARY KEY,
     KinoID BIGINT UNSIGNED NOT NULL,
     Saalname VARCHAR(50) NOT NULL,
+    Reihe INT UNSIGNED NOT NULL,
+    Sitze INT UNSIGNED NOT NULL,
     CONSTRAINT fk_KinoSaal FOREIGN KEY (KinoID) REFERENCES t_Kino(ID),
     UNIQUE uk_Saal (Saalname, KinoID)
-);
-
-CREATE TABLE IF NOT EXISTS t_Platz(
-    ID SERIAL PRIMARY KEY,
-    SaalID BIGINT UNSIGNED NOT NULL,
-    Reihe INT UNSIGNED NOT NULL,
-    Sitz INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_SitzSaal FOREIGN KEY (SaalID) REFERENCES t_Saal(ID),
-    UNIQUE uk_Sitz (Reihe, Sitz, SaalID)
 );
 
 CREATE TABLE IF NOT EXISTS t_Film (
@@ -68,47 +52,52 @@ CREATE TABLE IF NOT EXISTS t_FilmAuffuerung (
     ID SERIAL PRIMARY KEY,
     FilmID BIGINT UNSIGNED NOT NULL,
     SaalID BIGINT UNSIGNED NOT NULL,
-    Uhrzeit DATETIME NOT NULL,
+    AuffZeit DATETIME NOT NULL,
     CONSTRAINT fk_FilmAuff FOREIGN KEY (FilmID) REFERENCES t_Film(ID),
     CONSTRAINT fk_AuffSaal FOREIGN KEY (SaalID) REFERENCES t_Saal(ID),
-    UNIQUE uk_FilmAuffuerung (FilmID, SaalID, Uhrzeit)
+    UNIQUE uk_FilmAuffuerung (FilmID, SaalID, AuffZeit)
 );
 
-CREATE TABLE IF NOT EXISTS t_Kunde (
-    ID BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS t_Typ (
+    ID SERIAL PRIMARY KEY,
+    Typ VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS t_User (
+    ID SERIAL PRIMARY KEY,
     Benutzername VARCHAR (30) NOT NULL,
     Passwort VARCHAR(500) NOT NULL,
     Vorname VARCHAR (100) NOT NULL,
     Nachname VARCHAR (100) NOT NULL,
-    AdresseID BIGINT UNSIGNED NOT NULL,
     MailAdresse VARCHAR(100) NOT NULL,
-    CONSTRAINT fk_KundeAdresse FOREIGN KEY (AdresseID) REFERENCES t_Adresse(ID),
-    UNIQUE uk_UserAddress (AdresseID),
+    Strasse VARCHAR(250) NOT NULL,
+    StadtID BIGINT UNSIGNED NOT NULL,
+    TypID BIGINT UNSIGNED NOT NULL,
+    CONSTRAINT fk_UserAdresseStadt FOREIGN KEY (StadtID) REFERENCES t_Stadt(ID),
+    CONSTRAINT fk_UserTyp FOREIGN KEY (TypID) REFERENCES t_Typ(ID),
     UNIQUE uk_Username (Benutzername),
     UNIQUE uk_MailAdresse (MailAdresse)
-);
-
-CREATE TABLE IF NOT EXISTS t_Typ (
-    ID BIGINT UNSIGNED UNIQUE NOT NULL PRIMARY KEY,
-    Typ VARCHAR (30) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS t_Angestellte (
-    ID BIGINT UNSIGNED UNIQUE NOT NULL PRIMARY KEY,
-    Benutzername VARCHAR (30) NOT NULL,
-    Passwort VARCHAR(500) NOT NULL,
-    TypID BIGINT UNSIGNED NOT NULL,
-    CONSTRAINT fk_AngestelltenTyp FOREIGN KEY (TypID) REFERENCES t_Typ(ID)
 );
 
 CREATE TABLE IF NOT EXISTS t_Ticket (
     ID SERIAL PRIMARY KEY,
     AuffuerungID BIGINT UNSIGNED NOT NULL,
-    PlatzID BIGINT UNSIGNED NOT NULL,
+    Reihe INT UNSIGNED NOT NULL,
+	Platz INT UNSIGNED NOT NULL,
     Verkaufsdatum Date,
     KundeID BIGINT UNSIGNED NOT NULL,
     CONSTRAINT fk_AuffTicket FOREIGN KEY (AuffuerungID) REFERENCES t_FilmAuffuerung(ID),
-    CONSTRAINT fk_TicketPlatz FOREIGN KEY (PlatzID) REFERENCES t_Platz(ID),
-    CONSTRAINT fk_TicketKunde FOREIGN KEY (KundeID) REFERENCES t_Kunde(ID),
-    UNIQUE uk_Ticket (AuffuerungID, PlatzID)
+    CONSTRAINT fk_TicketKunde FOREIGN KEY (KundeID) REFERENCES t_User(ID),
+    UNIQUE uk_Ticket (AuffuerungID, Reihe, Platz)
 );
+
+CREATE OR REPLACE VIEW v_Account AS 
+	SELECT CONVERT(BenutzerName USING latin1) COLLATE latin1_general_cs AS Username, Passwort AS Password FROM t_User 
+    INNER JOIN t_Typ 
+    ON t_User.TypID = t_Typ.ID;
+    
+CREATE OR REPLACE VIEW v_KinoOrt AS
+	SELECT t_Kino.ID, t_Kino.Kinoname, t_Stadt.Ort FROM t_Kino INNER JOIN t_Stadt ON t_Kino.StadtID = t_Stadt.ID;
+    
+/*CREATE OR REPLACE VIEW v_FilmAuffuerung AS
+	SELECT ID t_FilmAuffuerung*/
