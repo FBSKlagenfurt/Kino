c77684898f7dde476b5d47cf3096321b8e4823cc
icke80 <icke8080@gmx.at>
2016-01-18 11:46:26 +0100

    Submitbutton link geändert

--- Website/EditCinema.php
+++ Website/EditCinema.php
@@ -79,10 +79,8 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <a style='color:#000;height:30px;padding-left:23px; display: block;background-repeat: no-repeat; background-size:100px 30px; background-image:url("/images/button_bg.png");background-repeat: no-repeat;' href="javascript:document.getElementById('cinemaForm').submit()">
-                                Speichern
-                            </a> 
-                            <input type="submit" style="visibility:hidden;width:0;height:0;"/>
+                            
+                            <input class="submitbutton" type="submit" value= "Speichern"/>
                         </td>
                     </tr>
                 </tbody>
