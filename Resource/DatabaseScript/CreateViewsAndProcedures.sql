473f50b5696a71f69ba547369ffad889cc8c57f0
icke80 <icke8080@gmx.at>
2016-01-25 15:34:33 +0100

    view update

--- Resource/DatabaseScript/CreateViewsAndProcedures.sql
+++ Resource/DatabaseScript/CreateViewsAndProcedures.sql
@@ -14,12 +14,12 @@ CREATE OR REPLACE VIEW v_Kino AS
 CREATE OR REPLACE VIEW v_Mitarbeiter AS
 	SELECT t_User.ID AS ID, t_User.Benutzername AS BN, t_User.Vorname AS VN, t_User.MailAdresse AS Mail, t_User.Nachname AS NN, t_User.Strasse AS STR , t_Stadt.PLZ AS PLZ, t_Stadt.Ort AS Ort, t_Typ.Typ FROM t_User INNER JOIN t_Stadt ON t_User.StadtID = t_Stadt.ID INNER JOIN t_Typ ON t_User.TypID = t_Typ.ID WHERE t_User.TypID = 2 OR t_User.TypID = 1;    
 
-/*CREATE OR REPLACE VIEW v_FilmAuffuerung AS
-	SELECT t_kino.Kinoname AS Kinoname, t_saal.Saalname AS Saalname, t_film.Titel AS Filmname, t_film.Dauer AS Dauer, t_FilmAuffuerung.AuffZeit AS Filmbeginn FROM t_kino INNER JOIN t_saal ON t_kino.ID = t_saal.KinoID INNER JOIN t_FilmAuffuerung ON t_saal.ID = t_FilmAuffuerung.SaalID INNER JOIN t_film ON t_FilmAuffuerung.FilmID = t_film.ID; 
-*/
 CREATE OR REPLACE VIEW v_FilmAuffuerung AS
-	SELECT t_FilmAuffuerung.ID AS AUFID, t_FilmAuffuerung.SaalID AS SaalID, t_FilmAuffuerung.FilmID AS FilmID, t_FilmAuffuerung.AuffZeit AS Wann FROM t_FilmAuffuerung;
+	SELECT t_kino.Kinoname AS Kinoname, t_saal.Saalname AS Saalname, t_film.Titel AS Filmname, t_film.Dauer AS Dauer, t_FilmAuffuerung.AuffZeit AS Filmbeginn FROM t_kino INNER JOIN t_saal ON t_kino.ID = t_saal.KinoID INNER JOIN t_FilmAuffuerung ON t_saal.ID = t_FilmAuffuerung.SaalID INNER JOIN t_film ON t_FilmAuffuerung.FilmID = t_film.ID; 
 
+/*CREATE OR REPLACE VIEW v_FilmAuffuerung AS
+	SELECT t_FilmAuffuerung.ID AS AUFID, t_FilmAuffuerung.SaalID AS SaalID, t_FilmAuffuerung.FilmID AS FilmID, t_FilmAuffuerung.AuffZeit AS Wann FROM t_FilmAuffuerung;
+*/
 DROP PROCEDURE IF EXISTS p_ManipulateCinema;
 
 DELIMITER $$
