ff94d40bd4f7e5eac1996713920c2b282b927009
icke80 <icke8080@gmx.at>
2016-01-28 15:25:21 +0100

    filmübersicht

--- Website/MovieOverview.php
+++ Website/MovieOverview.php
@@ -48,7 +48,38 @@
 
     <div class="page">
     	<div class="main">
-  
+            <h1 style="margin-left:auto;margin-right:auto;text-align:center;">Filme</h1>
+            <table class="table" style="margin-left:auto;margin-right:auto;">
+                <thead>
+                    <th>
+                        Filmtitel
+                    </th>
+                    <th>
+                        Dauer in Minuten
+                    </th>
+                    <th>
+                        Preis in €
+                    </th>
+                    <th>
+                        Kurzbeschreibung
+                    </th>
+                    
+                </thead>
+                <tbody>
+                    <?php
+                         require_once("getSqlConnection.php");
+                         $sqlcon = getSqlCon();
+                         $x = $sqlcon->prepare("SELECT * FROM t_Film");
+                         $x->execute();
+                         $x->bind_result($ID, $Titel, $Beschreibung, $Dauer, $Preis);
+                         while($x->fetch())
+                         {
+                             echo "<tr><td>$Titel</td><td>$Dauer</td><td>$Preis</td><td>$Beschreibung</td></tr>";
+                         }
+                         $sqlcon->close();
+                    ?>
+                </tbody>
+            </table>
 
         </div>
         <div class="clear">
